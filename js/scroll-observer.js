be8b65c6a002beeae9ff0136f7e3362e9f5ecf43
Angus Lo <szupie@gmail.com>
2025-08-03 23:25:01 -0400

    add parallax to galleries

--- js/scroll-observer.js
+++ js/scroll-observer.js
@@ -44,7 +44,46 @@ function initScrollObserver() {
 	observer.observe(postPeekElement);
 	stickyElement.style.setProperty('--scroll-grow-percentage', 0);
 
-	stickyElement.classList.add('observing')
+	stickyElement.classList.add('observing');
+
+
+	// scroll parallax
+	const parallaxWatchList = [];
+	function updateScrollPercent(node) {
+		const topPosition = document.documentElement.clientHeight - node.getBoundingClientRect().top;
+		const progress = topPosition / (node.clientHeight + document.documentElement.clientHeight);
+		node.style.setProperty('--scroll-progress', progress.toFixed(3));
+	}
+	function updateParallaxItemsInView() {
+		if (parallaxWatchList.length > 0) {
+			parallaxWatchList.forEach(node=>{ updateScrollPercent(node) });
+		}
+	}
+	function handleParallaxIntersect(entries) {
+		entries.forEach(entry => {
+			const node = entry.target;
+			if (entry.isIntersecting) {
+				updateScrollPercent(node);
+				parallaxWatchList.push(node);
+			} else {
+				const listIndex = parallaxWatchList.indexOf(node);
+				if (listIndex > -1) {
+					parallaxWatchList.splice(listIndex, 1);
+				}
+			}
+		})
+	}
+	const parallaxObserver = new IntersectionObserver(
+		handleParallaxIntersect, 
+		{ rootMargin: getComputedStyle(document.documentElement).getPropertyValue('--parallax-distance') }
+	);
+	if (document.querySelector('.gallery')) {
+		document.querySelectorAll('.gallery').forEach(
+			node => parallaxObserver.observe(node)
+		);
+		// scroll listener is needed because additional IntersectionObserver thresholds do not trigger while item is fully in view
+		window.addEventListener('scroll', updateParallaxItemsInView);
+	}
 }
 
 function clamp(min, value, max) {
